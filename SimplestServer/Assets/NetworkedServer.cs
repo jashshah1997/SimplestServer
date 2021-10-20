d5d0f7e2bace0272b15918dff5a08561f3391ac2
predator1245 <jashshah37@yahoo.com>
2021-10-20 19:55:35 +0200

    Support player Login and Create account requests.

--- SimplestServer/Assets/NetworkedServer.cs
+++ SimplestServer/Assets/NetworkedServer.cs
@@ -8,11 +8,13 @@ using UnityEngine.UI;
 
 public class NetworkedServer : MonoBehaviour
 {
+    private static string PlayerNamesFile = "PlayerAccounts.txt";
     int maxConnections = 1000;
     int reliableChannelID;
     int unreliableChannelID;
     int hostID;
     int socketPort = 5491;
+    LinkedList<PlayerAccount> playerAccounts;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +25,8 @@ public class NetworkedServer : MonoBehaviour
         unreliableChannelID = config.AddChannel(QosType.Unreliable);
         HostTopology topology = new HostTopology(config, maxConnections);
         hostID = NetworkTransport.AddHost(topology, socketPort, null);
-        
+        playerAccounts = new LinkedList<PlayerAccount>();
+        LoadPlayerAccounts();
     }
 
     // Update is called once per frame
@@ -68,6 +71,124 @@ public class NetworkedServer : MonoBehaviour
     private void ProcessRecievedMsg(string msg, int id)
     {
         Debug.Log("msg recieved = " + msg + ".  connection id = " + id);
+
+        string[] csv = msg.Split(',');
+        int signifier = int.Parse(csv[0]);
+
+        if (signifier == ClientToServerSignifiers.CreateAccount)
+        {
+            string username = csv[1];
+            string password = csv[2];
+
+            // Check for existing account
+            bool isUnique = true;
+            foreach(PlayerAccount pa in playerAccounts)
+            {
+                if (pa.name == username)
+                {
+                    isUnique = false;
+                    break;
+                }
+            }
+
+            // Add a new account if its unique
+            if (isUnique)
+            {
+                playerAccounts.AddLast(new PlayerAccount(username, password));
+                SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.Success, id);
+                SavePlayerAccounts();
+            }
+            else
+            {
+                SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.FailureNameInUse, id);
+            }
+        } 
+        else if (signifier == ClientToServerSignifiers.Login)
+        {
+            string username = csv[1];
+            string password = csv[2];
+
+            bool isFound = false;
+            foreach(PlayerAccount pa in playerAccounts)
+            {
+                if (pa.name == username)
+                {
+                    if (pa.password == password)
+                    {
+                        SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.Success, id);
+                    }
+                    else
+                    {
+                        SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.FailureIncorrectPassword, id);
+                    }
+                    isFound = true;
+                    
+                    break;
+                }
+            }
+
+            if (!isFound)
+            {
+                SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.FailureNameNotFound, id);
+            }
+        }
     }
 
+    private void SavePlayerAccounts()
+    {       
+        StreamWriter sw = new StreamWriter(Application.dataPath + Path.DirectorySeparatorChar + PlayerNamesFile);
+
+        foreach(PlayerAccount pa in playerAccounts)
+        {
+            sw.WriteLine(pa.name + "," + pa.password);
+        }
+        sw.Close();
+    }
+
+    private void LoadPlayerAccounts()
+    {
+        string path = Application.dataPath + Path.DirectorySeparatorChar + PlayerNamesFile;
+        if (File.Exists(path))
+        {
+            StreamReader sr = new StreamReader(path);
+            string line = "";
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] csv = line.Split(',');
+                playerAccounts.AddLast(new PlayerAccount(csv[0], csv[1]));
+            }
+            sr.Close();
+        }
+    }
+}
+public static class ClientToServerSignifiers
+{
+    public const int Login = 1;
+    public const int CreateAccount = 2;
+
+}
+
+public static class ServerToClientSignifiers
+{
+    public const int LoginResponse = 1;
+}
+
+public static class LoginResponses
+{
+    public const int Success = 1;
+    public const int FailureNameInUse = 2;
+    public const int FailureNameNotFound = 3;
+    public const int FailureIncorrectPassword = 4;
+}
+
+public class PlayerAccount
+{
+    public string name;
+    public string password;
+
+    public PlayerAccount(string Name, string Password)
+    {
+        name = Name;
+        password = Password;
+    }
 }
